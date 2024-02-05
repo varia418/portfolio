1b20e3086bb5099df367a38cd7a21f508690fcc5
varia <ltnhan2610@gmail.com>
2024-02-05 09:54:30 +0700

    add the rest of pages

---
+++ app/about/page.tsx
@@ -0,0 +1,3 @@
+export default function About() {
+	return <div className="mt-10 primary-color">ABOUT</div>;
+}
---
+++ app/contact/page.tsx
@@ -0,0 +1,3 @@
+export default function Contact() {
+	return <div className="mt-10 primary-color">Contact</div>;
+}
