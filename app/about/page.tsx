679a53cd7d25b19bfc328f05369d8ae5074b960b
varia <ltnhan2610@gmail.com>
2024-02-09 18:54:00 +0700

    add about page preliminary content

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,10 @@
 export default function About() {
-	return <div className="mt-10 primary-color">ABOUT</div>;
+	return (
+		<div className="mt-10 primary-color">
+			<h1>About me</h1>
+			<h2>Overview</h2>
+			<h2>Education</h2>
+			<h2>Experience</h2>
+		</div>
+	);
 }
