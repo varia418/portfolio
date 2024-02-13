f1d2fc5a0f66b3c9bc63dbf8b18b530ea865fe68
varia <ltnhan2610@gmail.com>
2024-02-13 11:51:02 +0700

    structure contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,18 @@
 export default function Contact() {
-	return <div className="mt-10 primary-color">Contact</div>;
+	return (
+		<div className="mt-10 primary-color">
+			<h1>Contact Me</h1>
+            <div>
+                <div>Email</div>
+                <div>Location</div>
+            </div>
+            <form>
+                <input placeholder="Name" />
+                <input placeholder="Email" />
+                <input placeholder="Subject" />
+                <textarea placeholder="Message" />
+                <button>Send Message</button>
+            </form>
+		</div>
+	);
 }
