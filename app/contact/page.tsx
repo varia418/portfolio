bab7f4836a3b4011124495ef2e524c92d9ae6e9d
varia <ltnhan2610@gmail.com>
2024-02-14 21:01:33 +0700

    apply grid to form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,15 +2,18 @@ export default function Contact() {
 	return (
 		<div className="mt-10 border bg-white rounded-lg p-10">
 			<h1 className="primary-color">Contact Me</h1>
-			<div>
-				<div>Email</div>
-				<div>Location</div>
+			<div className="flex justify-between">
+				<div className="grow">Email</div>
+				<div className="grow">Location</div>
 			</div>
-			<form>
+			<form className="grid grid-cols-2 gap-4">
 				<input className="border" placeholder="Name" />
 				<input className="border" placeholder="Email" />
-				<input className="border" placeholder="Subject" />
-				<textarea className="border" placeholder="Message" />
+				<input className="border col-span-full" placeholder="Subject" />
+				<textarea
+					className="border col-span-full"
+					placeholder="Message"
+				/>
 				<button className="bg-[--primary-color] border-2 border-[--primary-color] text-white rounded-full px-5 py-2 mt-4 hover:bg-slate-200 hover:text-[--primary-color] font-bold">
 					Send
 				</button>
