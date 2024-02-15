396277836611aedfadf06b01c119edfff17b299e
varia <ltnhan2610@gmail.com>
2024-02-15 22:15:39 +0700

    remote primary color on headlines

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,7 @@
 export default function About() {
 	return (
 		<div className="p-10 h-full relative">
-			<h1 className="primary-color mb-8 text-3xl font-bold relative">
+			<h1 className="mb-8 text-3xl font-bold relative">
 				About Me
 				<span className="absolute bg-[--primary-color] h-1 w-12 left-0 -bottom-2"></span>
 				<span className="absolute bg-[--primary-color] h-1 w-8 left-0 -bottom-4"></span>
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,7 +7,7 @@ export default function Contact() {
 	}
 	return (
 		<div className="p-10 h-full flex justify-center items-center relative">
-			<h1 className="primary-color mb-8 text-3xl font-bold absolute top-10 left-10">
+			<h1 className="mb-8 text-3xl font-bold absolute top-10 left-10">
 				Contact Me
 				<span className="absolute bg-[--primary-color] h-1 w-12 left-0 -bottom-2"></span>
 				<span className="absolute bg-[--primary-color] h-1 w-8 left-0 -bottom-4"></span>
