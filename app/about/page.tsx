a83bb5c7cda96c889c27c089e5425a7b290afa81
varia <ltnhan2610@gmail.com>
2024-02-15 22:14:04 +0700

    style about page headline

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,11 @@
 export default function About() {
 	return (
-		<div className="mt-10 primary-color">
-			<h1>About me</h1>
+		<div className="p-10 h-full relative">
+			<h1 className="primary-color mb-8 text-3xl font-bold relative">
+				About Me
+				<span className="absolute bg-[--primary-color] h-1 w-12 left-0 -bottom-2"></span>
+				<span className="absolute bg-[--primary-color] h-1 w-8 left-0 -bottom-4"></span>
+			</h1>
 			<h2>Overview</h2>
 			<h2>Education</h2>
 			<h2>Experience</h2>
