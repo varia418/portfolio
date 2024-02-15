11ce59a4191fe66efc32fc3138198671dc4fdb16
varia <ltnhan2610@gmail.com>
2024-02-15 10:26:22 +0700

    decorate page headline

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,11 @@
 export default function Contact() {
 	return (
 		<div className="mt-10 border bg-white rounded-lg p-10">
-			<h1 className="primary-color">Contact Me</h1>
+			<h1 className="primary-color mb-8 text-3xl font-bold relative ">
+				Contact Me
+				<span className="absolute bg-[--primary-color] h-1 w-12 left-0 -bottom-2"></span>
+				<span className="absolute bg-[--primary-color] h-1 w-8 left-0 -bottom-4"></span>
+			</h1>
 			<div className="flex justify-between">
 				<div className="grow">Email</div>
 				<div className="grow">Location</div>
