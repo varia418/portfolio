dcdcf691bad04111c14d487bf6e200d4ce9ac8a7
varia <ltnhan2610@gmail.com>
2024-02-16 10:05:17 +0700

    add skills page

---
+++ app/skills/page.tsx
@@ -0,0 +1,11 @@
+export default function Works() {
+	return (
+		<div className="p-10 h-full flex justify-center items-center relative">
+			<h1 className="mb-8 text-3xl font-bold absolute top-10 left-10">
+				Skills
+				<span className="absolute bg-[--primary-color] h-1 w-12 left-0 -bottom-2"></span>
+				<span className="absolute bg-[--primary-color] h-1 w-8 left-0 -bottom-4"></span>
+			</h1>
+		</div>
+	);
+}
