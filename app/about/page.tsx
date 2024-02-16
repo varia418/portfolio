2040999bdb8822387e205f2600c957043e3195fa
varia <ltnhan2610@gmail.com>
2024-02-16 08:39:01 +0700

    add overview content

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,33 @@
 export default function About() {
 	return (
-		<div className="p-10 h-full relative">
+		<div className="p-10 relative">
 			<h1 className="mb-8 text-3xl font-bold relative">
 				About Me
 				<span className="absolute bg-[--primary-color] h-1 w-12 left-0 -bottom-2"></span>
 				<span className="absolute bg-[--primary-color] h-1 w-8 left-0 -bottom-4"></span>
 			</h1>
-			<h2>Overview</h2>
-			<h2>Education</h2>
-			<h2>Experience</h2>
+			<div className="container">
+				<h2 className="text-xl font-semibold">Overview</h2>
+				<p>
+					I am a proficient full stack developer with diverse
+					expertise in the realm of web application development.
+					Possessing a comprehensive understanding of both the
+					front-end and back-end domains, I demonstrate proficiency in
+					conceiving and implementing intricate systems from inception
+					to fruition.
+				</p>
+				<p>
+					Throughout my educational and professional journey, I have
+					amassed knowledge across various technologies such as React,
+					Next.js, Express.js, MongoDB, and MySQL. I ardently embrace
+					learning opportunities and willingly subject myself to
+					challenges, continuously advancing my skills and fostering
+					creative capabilities in the construction of technological
+					products.
+				</p>
+				<h2 className="text-xl font-semibold">Education</h2>
+				<h2 className="text-xl font-semibold">Experience</h2>
+			</div>
 		</div>
 	);
 }
